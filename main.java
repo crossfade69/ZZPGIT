d057a0a22cfea24b0a62fc5c23231a4487749111
Varrial <73936910+Varrial@users.noreply.github.com>
2022-10-18 10:05:59 +0200

    duuuupa

--- main.java
+++ main.java
@@ -2,6 +2,6 @@ class main{
 	static{
 		System.out.println("STATIC");}
 	public static void main(String args[]){
-		System.out.println("Static method");
+		System.out.println("Static method lol");
 	}
 }
